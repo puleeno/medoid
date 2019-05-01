21d4a647a00bb06289dd368ad6b57d9e19976c4e
Puleeno Nguyen <puleeno@gmail.com>
2019-05-01 19:26:52 +0700

    add plugin file

---
+++ medoud.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Medoud
+ * Plugin URI: https://puleeno.com
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: Replacement WordPress media with Cloud Storage and CDN
+ * Tags: media, clouds, cdn, multi-cloud, synchonize
+ */
+
