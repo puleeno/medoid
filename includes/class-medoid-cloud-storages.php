c30dd035cc0cc3c824c1b25877aa060f2367fe66
Puleeno Nguyen <puleeno@gmail.com>
2020-02-08 00:14:21 +0700

    Support hard code active clouds

--- includes/class-medoid-cloud-storages.php
+++ includes/class-medoid-cloud-storages.php
@@ -2,8 +2,23 @@
 
 class Medoid_Cloud_Storages {
 	protected static $clouds = [];
-
 	protected $query;
+
+	public function __construct() {
+		$this->setup_clouds();
+	}
+
+	public function setup_clouds() {
+		$options = array();
+		$cloud   = new Medoid_Cloud_Backblaze( 1 );
+		if ( $cloud instanceof Medoid_Cloud ) {
+			self::$clouds[1] = $cloud;
+		}
+	}
+
+	public static function getDefaultCloud() {
+		return self::getClouds( 1 );
+	}
 
 	public static function getClouds( $id = null ) {
 		if ( is_null( $id ) ) {
@@ -15,20 +30,8 @@ class Medoid_Cloud_Storages {
 		return false;
 	}
 
-	public function __construct() {
-		$this->setup_clouds();
-	}
-
-	public static function getDefaultCloud() {
-		return self::getClouds( 1 );
-	}
-
-	public function setup_clouds() {
-		$options = array();
-		$cloud   = new Medoid_Cloud_Backblaze( 1 );
-		if ( $cloud instanceof Medoid_Cloud ) {
-			self::$clouds[1] = $cloud;
-		}
+	public static function getActiveClouds() {
+		return [ 1 => 'Default' ];
 	}
 }
 
