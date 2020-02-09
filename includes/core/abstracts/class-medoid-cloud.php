7507b3df8ffc89724013ed535f2a6662478178bb
Puleeno Nguyen <puleeno@gmail.com>
2020-02-10 00:17:56 +0700

    Optimize load medoid db

--- includes/core/abstracts/class-medoid-cloud.php
+++ includes/core/abstracts/class-medoid-cloud.php
@@ -12,9 +12,15 @@ abstract class Medoid_Cloud implements Medoid_Cloud_Interface {
 		return $this->_internal_cloud_id;
 	}
 
+	public function get_db() {
+		if ( empty( $this->db ) ) {
+			$this->db = Medoid_Core_Db::instance();
+		}
+		return $this->db;
+	}
+
 	public function sync_to_cloud( $limit_items = 50 ) {
-		$this->db = Medoid_Core_Db::instance();
-		$images   = $this->db->get_images(
+		$images = $this->get_db()->get_images(
 			array(
 				'cloud_id'    => $this->get_id(),
 				'is_uploaded' => false,
