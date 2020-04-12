631bfa95ecdd4d1a363602ab9474e1aaeccc47ed
Puleeno Nguyen <puleeno@gmail.com>
2020-04-12 16:30:31 +0700

    add view file Ajax

--- includes/core/class-medoid-image-proxy.php
+++ includes/core/class-medoid-image-proxy.php
@@ -1,5 +1,16 @@
 <?php
 class Medoid_Image_Proxy {
+	public function __construct() {
+		$this->init_hooks();
+	}
+
+	public function init_hooks() {
+		add_action( 'wp_ajax_medoid_view_file', array( $this, 'view_file' ) );
+		add_action( 'wp_ajax_nopriv_medoid_view_file', array( $this, 'view_file' ) );
+	}
+
+	public function view_file() {
+	}
 }
 
 new Medoid_Image_Proxy();
