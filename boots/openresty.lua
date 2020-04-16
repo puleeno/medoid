3eb26ad557e32196f3cee16754b1532109ad3ee8
Puleeno Nguyen <puleeno@gmail.com>
2020-04-16 23:13:53 +0700

    Update proxy image URL

--- boots/openresty.lua
+++ boots/openresty.lua
@@ -4,12 +4,23 @@ function medoid.access(prefix, host)
     -- ngx.header["Content-Type"] = "text/html"
     host = host or ngx.var.host
 
-    local image_id = ngx.var.request_uri
+    local image_id = ngx.var.uri:gsub('/?image/', '')
+    local query_str = ''
 
-    ngx.var.target = string.format('%s://%s', ngx.var.scheme, host);
+    if ngx.var.query_string then
+        query_str = '&' .. ngx.var.query_string
+    end
 
-    ngx.say(ngx.var.target)
-    ngx.exit(ngx.HTTP_OK)
+    ngx.var.target = string.format(
+        '%s://%s/wp-admin/admin-ajax.php?action=medoid_view_file&id=%s%s',
+        ngx.var.scheme,
+        host,
+        image_id,
+        query_str
+    );
+
+    -- ngx.say(ngx.var.target)
+    -- ngx.exit(ngx.HTTP_OK)
 end
 
 return medoid
