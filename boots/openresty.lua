9229fbaa03632a9c2228064d0367b1b2165cddef
Puleeno Nguyen <puleeno@gmail.com>
2020-04-18 09:57:52 +0700

    Support load image via proxy

--- boots/openresty.lua
+++ boots/openresty.lua
@@ -1,16 +1,12 @@
-require("DBI")
+local DBI = require("DBI")
 
 local medoid = {
     configs = {
         db = {}
     },
-    use_placeholder = true,
-    connection = nil
+    connection = nil,
+    use_placeholder = true
 }
-
-function medoid.open_db(self)
-    
-end
 
 function medoid.parse_config_file(self, app_root)
     local wp_config = io.open(string.format("%s/wp-config.php", app_root), "r")
@@ -23,23 +19,62 @@ function medoid.parse_config_file(self, app_root)
             elseif db_key == "DB_USER" then
                 self.configs.db.user = config_value
             elseif db_key == "DB_PASSWORD" then
-                self.configs.db.pass = config_value
+                self.configs.db.password = config_value
             elseif db_key == "DB_HOST" then
                 self.configs.db.host = config_value
             elseif db_key == "DB_CHARSET" then
                 self.configs.db.charset = config_value
             end
         end
+
+        local db_prefix = all_configs:match("$table_prefix%s?=%s?'([^']+)")
+        self.configs.db.prefix = db_prefix
     else
         ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
     end
 end
 
+function medoid.open_db(self)
+    self.connection =
+        assert(
+        DBI.Connect("MySQL", self.configs.db.name, self.configs.db.user, self.configs.db.password, self.configs.db.host)
+    )
+end
+
 function medoid.close_db(self)
+    if self.connection == nil then
+        return
+    end
+
+    self.connection:close()
 end
 
 function medoid.get_image(self, proxy_image_id)
-    return true
+    local sth =
+        assert(
+        self.connection:prepare(
+            [[
+                SELECT
+                    i.post_id,
+                    i.image_url,
+                    s.image_url AS image_size_url,
+                    p.guid
+                FROM
+                    lc_medoid_images i
+                    LEFT JOIN lc_medoid_image_sizes s ON s.image_id = i.ID
+                    INNER JOIN lc_posts p ON p.ID = i.post_id
+                WHERE
+                    i.proxy_id = ?
+                    OR s.proxy_id = ?
+            ]]
+        )
+    )
+
+    -- execute select with a bind variable
+    sth:execute(proxy_image_id, proxy_image_id)
+
+    -- iterate over the returned data
+    return sth:fetch()
 end
 
 function medoid.placeholder_image(self)
@@ -52,26 +87,32 @@ function medoid.access(self, realpath_root, prefix, host)
     self.open_db(self)
 
     local image_id = ngx.var.uri:gsub("/?image/", "")
-    local image = self.get_image(self,image_id)
-    local image_url = ''
+    local image = self.get_image(self, image_id)
+    local image_url = ""
 
     if image == nil then
         if self.configs.use_placeholder then
             image_url = self:placeholder_image()
         else
+            self.close_db(self)
             ngx.exit(ngx.HTTP_NOT_FOUND)
         end
     end
 
-    local query_str = ""
-        if ngx.var.query_string then
-            query_str = "&" .. ngx.var.query_string
-        end
+    if image[3] ~= "" then -- Check image size URL
+        image_url = image[3]
+    elseif image[2] ~= "" then -- Check image URL
+        image_url = image[2]
+    else
+        image_url = image[4] -- Use guid if all image URL is not exists
+    end
 
-    -- ngx.say(ngx.var.target)
-    -- ngx.exit(ngx.HTTP_OK)
+    if ngx.var.query_string then
+        image_url = image_url .. "&" .. ngx.var.query_string
+    end
 
-    -- ngx.var.target = 'https://www.google.com/logos/doodles/2020/thank-you-teachers-and-childcare-workers-6753651837108762.3-law.gif'
+    self.close_db(self)
+    ngx.var.target = image_url
 end
 
 return medoid
