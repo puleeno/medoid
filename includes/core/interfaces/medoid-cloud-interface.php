625de2ee1608c89d03f822d84201c2043a0eb781
Puleeno Nguyen <puleeno@gmail.com>
2020-06-05 07:36:52 +0700

    Add new method to cloud storage interface

--- includes/core/interfaces/medoid-cloud-interface.php
+++ includes/core/interfaces/medoid-cloud-interface.php
@@ -2,6 +2,8 @@
 interface Medoid_Cloud_Interface {
 	public function __construct( $id, $configs = array() );
 
+	public function get_name();
+
 	public function get_id();
 
 	public function upload( $file, $new_file );
