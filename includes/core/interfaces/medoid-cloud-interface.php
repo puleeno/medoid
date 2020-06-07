e3e81521541ddc161ff58543fde3952a8d32c36b
Puleeno Nguyen <puleeno@gmail.com>
2020-06-07 21:41:10 +0700

    Update cloud interfaces

--- includes/core/interfaces/medoid-cloud-interface.php
+++ includes/core/interfaces/medoid-cloud-interface.php
@@ -6,7 +6,7 @@ interface Medoid_Cloud_Interface {
 
 	public function upload( $file, $new_file );
 
-	public function is_exists( $file);
+	public function is_exists( $file );
 
-	public function delete_file( $file_id);
+	public function delete_image( $image, $delete_cloud_file = true, $delete_attachment = true, $soft_delete = false );
 }
