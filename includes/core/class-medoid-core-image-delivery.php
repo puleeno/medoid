773815723feaacef2deb90f16c5fccd29b32626b
Puleeno Nguyen <puleeno@gmail.com>
2020-09-22 01:03:29 +0700

    Use CDN for prepare JSON

--- includes/core/class-medoid-core-image-delivery.php
+++ includes/core/class-medoid-core-image-delivery.php
@@ -20,6 +20,11 @@ class Medoid_Core_Image_Delivery {
 
 	public function prepare_json( $response, $attachment, $meta ) {
 		$thumbnail = wp_get_attachment_image_src( $attachment->ID, array( 150, 150 ) );
+		$full_url  = wp_get_attachment_image_src( $attachment->ID);
+
+		if (isset($full_url[0])  && is_a($full_url[0], Medoid_Image::class)) {
+			$response['url'] = (string)$full_url[0]->to_string();
+		}
 
 		if ( $thumbnail ) {
 			$thumbnail_url    = (string) $thumbnail[0];
