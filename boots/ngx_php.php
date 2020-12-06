ffcfa5e3e0fdbcf1d30631b75cfe856d0bc6df9c
Puleeno Nguyen <puleeno@gmail.com>
2020-12-06 19:05:05 +0700

    Added test file to dev

--- boots/ngx_php.php
+++ boots/ngx_php.php
@@ -6,9 +6,30 @@
  */
 class Medoid_Ngx_PHP {
 	protected $image_url;
+	protected $conn;
 
 	// Load WordPress
 	public function __construct() {
+		// Load WordPress constants
+		$wp_config = sprintf( '%s/wp-config.php', ngx_request_document_root() );
+		if ( ! file_exists( $wp_config ) ) {
+			return;
+		}
+		require_once $wp_config;
+
+		$servername = constant( 'DB_HOST' );
+		$username   = constant( 'DB_USER' );
+		$password   = constant( 'DB_PASSWORD' );
+		$dbname     = constant( 'DB_NAME' );
+
+		try {
+			$conn = new PDO( "mysql:host=$servername;dbname=$dbname", $username, $password );
+			// set the PDO error mode to exception
+			$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			echo 'Connected successfully';
+		} catch ( PDOException $e ) {
+			echo 'Connection failed: ' . $e->getMessage();
+		}
 	}
 
 	public function load_image_url_from_database() {
---
+++ boots/ngx_php_test.php
@@ -0,0 +1,38 @@
+<?php
+global $is_exit;
+if ( defined( 'NGX_HTTP_NOT_FOUND' ) ) {
+	define( 'NGX_HTTP_NOT_FOUND', 'NGX_HTTP_NOT_FOUND' );
+}
+
+if ( ! function_exists( 'ngx_request_document_root' ) ) {
+	function ngx_request_document_root() {
+		return realpath( '../../../../' );
+	}
+}
+
+if ( ! function_exists( 'ngx_exit' ) ) {
+	function ngx_exit( $status = true ) {
+		global $is_exit;
+
+		$is_exit = $status;
+	}
+}
+
+if ( ! class_exists( 'ngx_var' ) ) {
+	class ngx_var {
+		public static $vars = array();
+		public static function set( $name, $value ) {
+			static::$vars[ $name ] = $value;
+		}
+	}
+}
+
+require_once dirname( __FILE__ ) . '/ngx_php.php';
+
+$ngx_php = new Medoid_Ngx_PHP();
+$ngx_php->load_image_url_from_database();
+$ngx_php->set_image_url();
+
+var_dump( 'Image URL:' . $image_url );
+var_dump( 'Is exit:' . $is_exit );
+var_dump( 'Variables:', var_export( ngx_var::$vars, true ) );
