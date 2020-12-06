f8f405e0bb1aeabcc7db96d94e09304213a45c44
Puleeno Nguyen <puleeno@gmail.com>
2020-12-06 20:11:36 +0700

    change driver to mysqli

--- boots/ngx_php.php
+++ boots/ngx_php.php
@@ -22,14 +22,14 @@ class Medoid_Ngx_PHP {
 		$password   = constant( 'DB_PASSWORD' );
 		$dbname     = constant( 'DB_NAME' );
 
-		try {
-			$conn = new PDO( "mysql:host=$servername;dbname=$dbname", $username, $password );
-			// set the PDO error mode to exception
-			$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-			echo 'Connected successfully';
-		} catch ( PDOException $e ) {
-			echo 'Connection failed: ' . $e->getMessage();
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password);
+
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
 		}
+		echo "Connected successfully";
 	}
 
 	public function load_image_url_from_database() {
